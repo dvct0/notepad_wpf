06bd4a3c9dbee6168ae49c87578d1158ef9b4bd8
dvct0 <sema.shm2@gmail.com>
2021-08-13 21:31:15 +0500

    fix button "create" and minor fix for design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,68 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace notepad_wpf
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<FileInfo> Notes = new ObservableCollection<FileInfo>();
+        DirectoryInfo RootDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+        string NotesDirectory;
+
         public MainWindow ()
         {
             InitializeComponent();
+            listOfNotes.ItemsSource = Notes;
+
+            //check and create a folder for notes
+            NotesDirectory = RootDir.ToString() + "/notes/";
+            if (Directory.Exists(NotesDirectory) == false)
+            {
+                Directory.CreateDirectory(NotesDirectory);
+            }
+
+            GetNotesList();
+        }
+
+        private void GetNotesList ()//get a list with notes
+        {
+            Notes.Clear();
+            DirectoryInfo dir = new DirectoryInfo(NotesDirectory);
+            FileInfo[] files = dir.GetFiles("*.html");
+            foreach (var item in files)
+            {
+                Notes.Add(item);
+            }
+        }
+
+        private void Create (object sender, RoutedEventArgs e)//Create a new note
+        {
+            try
+            {
+                File.Create(NotesDirectory + "/" + NameForNewNote.Text + ".html");
+            }
+            catch
+            {
+
+            }
+            GetNotesList();
+        }
+
+        private void Delete (object sender, RoutedEventArgs e)//Delete selected note
+        {
+
+        }
+
+        private void Edit (object sender, RoutedEventArgs e)//Edit selected note
+        {
+
+        }
+
+        private void Save (object sender, RoutedEventArgs e)//Save selected note
+        {
+
         }
     }
 }
